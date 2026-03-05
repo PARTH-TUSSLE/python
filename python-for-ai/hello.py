23af7d7bc670c92da59e1aeb8e97f5aa1caeaa29
PARTH-TUSSLE <parthgartan26feb@gmail.com>
2026-03-05 19:20:27 +0530

    conditionals, loops, sets, tuples, lists, dictionaries

--- python-for-ai/hello.py
+++ python-for-ai/hello.py
@@ -1,4 +1,50 @@
-import requests
+name = "Shivang"
+#dynamic values
+print(f"My name is {name}")#fstrings
+temp = 31
+if temp > 30:
+  print("VERY HOT")
+elif temp >25:
+  print("HOT")
+else:
+  print("NOT HOT")
 
-response = requests.get("https://api.github.com")
-print(response.status_code)
+
+for i in range(7):
+  print(i)
+
+for i in range(1,7): #last idx is exclusive
+  print(i)
+
+for i in range(0,10,2):
+  print(i)
+
+#LISTS
+
+isTrue = False
+my_list = ["Parth", 25, True, isTrue]
+name = my_list[0]
+print(name);
+
+#DICTIONARIES
+my_dict = {
+  "name": "Parth",
+  "age": 25,
+  "isAdult": True
+}
+
+# del my_dict["age"]
+
+print(my_dict["age"])
+
+#TUPLES -> Immutable
+
+my_tuple = (3,5, "Parth")
+
+#SETS
+
+my_set = set(["hello", 1, False])
+my_set2 = {"hello", 1, False}
+scores = {40,40} # Duplicates not allowed
+
+print(scores)
